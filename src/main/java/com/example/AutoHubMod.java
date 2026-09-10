b9a150a1a57eab50870f5b7ad00dad7c991c1a30
zontiikxd-glitch <zontiikxd@gmail.com>
2026-09-10 20:42:58 +0300

    Update AutoHubMod.java

--- src/main/java/com/example/AutoHubMod.java
+++ src/main/java/com/example/AutoHubMod.java
@@ -3,16 +3,19 @@ package com.example;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.PlayerManager;
 
 public class AutoHubMod implements ModInitializer {
     @Override
     public void onInitialize() {
         ServerTickEvents.END_SERVER_TICK.register(server -> {
-            for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
-                if (!server.getPlayerManager().isWhitelisted(player.getGameProfile())) {
-                    for (ServerPlayerEntity other : server.getPlayerManager().getPlayerList()) {
+            PlayerManager playerManager = server.getPlayerManager();
+            for (ServerPlayerEntity player : playerManager.getPlayers()) {
+                // Проверяем вайтлист через getUserWhiteList()
+                if (!playerManager.getUserWhiteList().isWhitelisted(player.getGameProfile())) {
+                    for (ServerPlayerEntity other : playerManager.getPlayers()) {
                         if (player != other && player.distanceTo(other) <= 35.0D) {
-                            server.getCommandManager().executeWithPrefix(player.getCommandSource(), "hub");
+                            server.getCommandManager().getDispatcher().execute("hub", player.getCommandSource());
                             break;
                         }
                     }
