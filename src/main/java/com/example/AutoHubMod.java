76bee9e9266d4407de501830d396be5174877099
zontiikxd-glitch <zontiikxd@gmail.com>
2026-09-10 20:51:35 +0300

    Update AutoHubMod.java

--- src/main/java/com/example/AutoHubMod.java
+++ src/main/java/com/example/AutoHubMod.java
@@ -11,11 +11,11 @@ public class AutoHubMod implements ModInitializer {
         ServerTickEvents.END_SERVER_TICK.register(server -> {
             PlayerManager playerManager = server.getPlayerManager();
             for (ServerPlayerEntity player : playerManager.getPlayers()) {
-                // Проверяем вайтлист через getUserWhiteList()
                 if (!playerManager.getUserWhiteList().isWhitelisted(player.getGameProfile())) {
                     for (ServerPlayerEntity other : playerManager.getPlayers()) {
                         if (player != other && player.distanceTo(other) <= 35.0D) {
-                            server.getCommandManager().getDispatcher().execute("hub", player.getCommandSource());
+                            // Правильный вызов команды через ServerCommandManager
+                            server.getCommandManager().executeWithPrefix(player.getCommandSource(), "hub");
                             break;
                         }
                     }
